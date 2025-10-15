50739046e8767ed59e3f7a415e3d6bdb03451d2e
FernandoMachado27 <fernando2747@yahoo.com>
2025-10-14 22:38:51 -0300

    Gerar relatórios e playback

--- cypress.config.js
+++ cypress.config.js
@@ -6,5 +6,12 @@ module.exports = defineConfig({
       // implement node event listeners here
     },
     //video: true, // cypress grava os testes em vídeo
+    reporter: 'mochawesome',
+    reporterOptions: {
+      reportDir: 'cypress/results',
+      overwrite: false,
+      html: true,
+      json: false,
+      timestamp: "mmddyyyy_HHMMss" }
   },
 });
