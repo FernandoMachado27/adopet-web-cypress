b7ec729ce7477785b63924c13351f6587d8e25de
FernandoMachado27 <fernando2747@yahoo.com>
2025-10-14 22:27:18 -0300

    Add videos cypress

--- cypress.config.js
+++ cypress.config.js
@@ -5,5 +5,6 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    //video: true, // cypress grava os testes em vídeo
   },
 });
